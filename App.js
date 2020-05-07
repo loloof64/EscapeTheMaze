76ae94af52a4b665db9e653db9adbabe7dd34c57
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-05-07 19:21:25 +0200

    Testing 3D with a spinning cube

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View as GraphicsView } from 'expo-graphics';
+
+import ExpoTHREE, { THREE } from 'expo-three';
 
 export default function App() {
+
+  let scene, camera, renderer;
+  let cube;
+
+  const buildCubeForScene = (scene) => {
+    let geometry = new THREE.BoxGeometry(1, 1, 1);
+    let material = new THREE.MeshBasicMaterial({color: 0x0000FC});
+
+    let cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    return cube;
+  }
+
+  const onContextCreate = async ({
+    gl,
+    width,
+    height,
+    scale: pixelRatio,
+  }) => {
+    renderer = new ExpoTHREE.Renderer({ gl, pixelRatio, width, height });
+    renderer.setClearColor(0x000000)
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    camera.position.z = 5;
+    
+    cube = buildCubeForScene(scene);
+  };
+
+  const onRender = delta => { 
+    cube.rotation.y += delta * 0.5;
+    renderer.render(scene, camera);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <GraphicsView
+      onContextCreate={onContextCreate}
+      onRender={onRender}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Escape maze
 
 Escape the maze
+
+## Credits
+
+Icon and splash screen use business.png from Those Icons, and donwloaded on flaticon.com
