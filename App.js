4a72a10a9f5c269376d6b25fc1d4d99e9f582040
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-05-07 22:21:36 +0200

    Screen is split up into 2 zones: 3d part above and 2 part (wip) below

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { View as GraphicsView } from 'expo-graphics';
+import { Dimensions, View } from 'react-native';
 
+import { GLView } from 'expo';
+import Expo2DContext from 'expo-2d-context';
+import { View as GraphicsView } from 'expo-graphics';
 import ExpoTHREE, { THREE } from 'expo-three';
+import { ProcessingView } from 'expo-processing';
 
 export default function App() {
 
-  let scene, camera, renderer;
+  let scene, camera, renderer, gl, scale;
   const InitialCameraZ = 25;
+
+  const width = Dimensions.get('window').width;
+  const height = Dimensions.get('window').height;
 
   const buildMazeForScene = (scene) => {
     for (let i = 0; i < 10; i++) {
@@ -30,14 +37,22 @@ export default function App() {
     return wall;
   }
 
-  const onContextCreate = async ({
-    gl,
+  const configureRenderer = ({ width, height }) => {
+    renderer = new ExpoTHREE.Renderer({ gl, pixelRatio: scale, width, height });
+    renderer.setClearColor(0x000000)
+  }
+
+  const on3DContextCreate = async ({
+    gl: tempGl,
     width,
     height,
     scale: pixelRatio,
   }) => {
-    renderer = new ExpoTHREE.Renderer({ gl, pixelRatio, width, height });
-    renderer.setClearColor(0x000000)
+    gl = tempGl;
+    scale = pixelRatio;
+
+    configureRenderer({ width, height });
+
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
     camera.position.x = 3;
@@ -52,10 +67,33 @@ export default function App() {
     renderer.render(scene, camera);
   };
 
+  const onResize = ({width, height}) => {
+    configureRenderer({ width, height });
+  }
+
+  const sketch2D = (p) => {
+    p.setup = () => {
+      p.size(width, height * 0.5);
+      p.background(152);
+    }
+
+    p.draw = () => {
+
+    }
+  }
+
   return (
-    <GraphicsView
-      onContextCreate={onContextCreate}
-      onRender={onRender}
-    />
+    <View style={{width, height}}>
+      <GraphicsView
+        style={{width, height: height * 0.5}}
+        onContextCreate={on3DContextCreate}
+        onRender={onRender}
+        onResize={onResize}
+      />
+      <ProcessingView
+        style={{width, height: height * 0.5}}
+        sketch={sketch2D}
+      />
+    </View>
   );
 }
