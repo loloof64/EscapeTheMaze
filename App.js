80c7302ea4df912bae27fd032faadb3888e05d60
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-05-08 01:19:33 +0200

    Added controls for moving into the map: still buggy

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dimensions, View } from 'react-native';
+import { Dimensions, View, Image, TouchableHighlight } from 'react-native';
 
 import { View as GraphicsView } from 'expo-graphics';
 import ExpoTHREE, { THREE } from 'expo-three';
@@ -7,26 +7,62 @@ import { ProcessingView } from 'expo-processing';
 
 export default function App() {
 
+  const mapDimension = 16;
+  const wallThickness = 4;
+  const wallLength = 4;
   let scene, camera, renderer, gl, scale, twoDZoneProgressionRate;
-  const InitialCameraZ = 20;
+  let map, mapDrawn;
+  const mapBackground = 152;
+  let playerX, playerY, prevPlayerX, prevPlayerY;
+  const InitialCameraX = wallThickness; // we must initially place on row 1
+  const InitialCameraZ = 0;
 
   const width = Dimensions.get('window').width;
   const height = Dimensions.get('window').height;
 
-  const buildMazeForScene = (scene) => {
-    for (let i = 0; i < 10; i++) {
-      let wallLeft = createWallForScene(scene);
-      wallLeft.position.x = -2;
-      wallLeft.position.z = 20 - 4*i;
+  const buildMap = () => {
 
-      let wallRight = createWallForScene(scene);
-      wallRight.position.x = 8;
-      wallRight.position.z = 20 - 4*i;
+    const buildMapEmptyArray = () => {
+      map = new Array(mapDimension);
+      for (let j = 0; j < mapDimension; j++) {
+        const arrToSet = new Array(mapDimension);
+        for (let i = 0; i < mapDimension; i++) {
+          arrToSet[i] = 0;
+        }
+        map[j] = arrToSet;
+      }
+    }
+
+    const createFreeZonesForMap = () => {
+      for (let i = 0; i < mapDimension; i++) map[1][i] = 1;
+    }
+
+    prevPlayerX = 0;
+    prevPlayerY = 1;
+    playerX = 0;
+    playerY = 1;
+    mapDrawn = false;
+    buildMapEmptyArray();
+    createFreeZonesForMap();
+  }
+
+  const buildMazeForScene = (scene) => {
+    for (let j = 0; j < mapDimension; j++) {
+      for (let i = 0; i < mapDimension; i++) {
+        const cellValue = map[j][i];
+        const isAWall = cellValue === 0;
+
+        if (isAWall) {
+          let wall = createWallForScene(scene);
+          wall.position.x = wallThickness * j;
+          wall.position.z = - wallThickness * i;
+        }
+      }
     }
   }
 
   const createWallForScene = (scene) => {
-    const geometry = new THREE.BoxGeometry(2, 16, 4);
+    const geometry = new THREE.BoxGeometry(wallThickness, 16, wallLength);
     const material = new THREE.MeshBasicMaterial({color: 0x559641});
 
     const wall = new THREE.Mesh(geometry, material);
@@ -53,16 +89,18 @@ export default function App() {
 
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    camera.position.x = 3;
+    camera.position.x = InitialCameraX;
     camera.position.z = InitialCameraZ;
     
+    buildMap();
     buildMazeForScene(scene);
   };
 
   const onRender = delta => { 
-    camera.position.z -= delta * 3.5;
-    twoDZoneProgressionRate = 20 - camera.position.z;
-    if (camera.position.z <= 0) camera.position.z = InitialCameraZ;
+    prevPlayerX = playerX;
+    prevPlayerY = playerY;
+
+    playerX = camera.position.z * -1;
     renderer.render(scene, camera);
   };
 
@@ -70,8 +108,86 @@ export default function App() {
     configureRenderer({ width, height });
   }
 
+  const goUp = () => {
+    if (playerY === 0) return;
+    const nextCellValue = map[playerY-1][playerX];
+    const nextCellIsAWall = nextCellValue === 0;
+
+    if (nextCellIsAWall) return;
+    prevPlayerY = playerY;
+    playerY--;
+    camera.position.x -= wallThickness;
+  }
+
+  const goDown = () => {
+    if (playerY === mapDimension - 1) return;
+    const nextCellValue = map[playerY+1][playerX];
+    const nextCellIsAWall = nextCellValue === 0;
+
+    if (nextCellIsAWall) return;
+    prevPlayerY = playerY;
+    playerY++;
+    camera.position.x += wallThickness;
+  }
+
+  const goLeft = () => {
+    if (playerX === 0) return;
+    const nextCellValue = map[playerY][playerX-1];
+    const nextCellIsAWall = nextCellValue === 0;
+
+    if (nextCellIsAWall) return;
+    prevPlayerX = playerX;
+    playerX -= 1;
+    camera.position.z += wallThickness;
+  }
+
+  const goRight = () => {
+    if (playerX === mapDimension - 1) return;
+    const nextCellValue = map[playerY][playerX+1];
+    const nextCellIsAWall = nextCellValue === 0;
+
+    if (nextCellIsAWall) return;
+    prevPlayerX = playerX;
+    playerX+=1;
+    camera.position.z -= wallThickness;
+  }
+
   const sketch2D = (p) => {
     const unit = width * 0.04;
+
+    const drawMapIfNecessary = () => {
+      if (map && !mapDrawn)
+      {
+        p.background(mapBackground);
+        
+        p.stroke(0);
+        for (let j = 0; j < mapDimension; j++)
+        {
+          for (let i = 0; i < mapDimension; i++)
+          {
+            const cellValue = map[j][i];
+            const isAWall = cellValue === 0;
+
+            if (isAWall)
+            {
+              p.point(unit * (3+i), unit * (3+j));
+            }
+          }
+        }
+
+        mapDrawn = true;
+      }
+    }
+
+    const erasePreviousPlayerPosition = () => {
+      p.stroke(mapBackground);
+      p.point(unit*(3+prevPlayerX), unit*(3+prevPlayerY));
+    }
+
+    const drawNewPlayerPosition = () => {
+      p.stroke(255, 12, 20);
+      p.point(unit*(3+playerX), unit*(3+playerY));
+    }
 
     p.setup = () => {
       p.size(width, height * 0.5);
@@ -79,14 +195,9 @@ export default function App() {
     }
 
     p.draw = () => {
-      p.background(152);
-
-      p.stroke(0);
-      p.line(3*unit, 3*unit, 23*unit, 3*unit);
-      p.line(3*unit, 7*unit, 23*unit, 7*unit);
-
-      p.stroke(255, 12, 20);
-      p.point(3*unit + twoDZoneProgressionRate*unit, 5*unit);
+      drawMapIfNecessary();
+      drawNewPlayerPosition();
+      erasePreviousPlayerPosition();
     }
   }
 
@@ -98,10 +209,77 @@ export default function App() {
         onRender={onRender}
         onResize={onResize}
       />
-      <ProcessingView
+      <View
         style={{width, height: height * 0.5}}
-        sketch={sketch2D}
-      />
+      >
+        <ProcessingView
+          style={{width, height: height * 0.5}}
+          sketch={sketch2D}
+        />
+        <View
+          style={{
+            width, height: height * 0.5,
+            zIndex: 1,
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            alignItems: 'flex-start',
+            position: "absolute",
+          }}
+        >
+          <TouchableHighlight
+            style={{
+              position: 'absolute',
+              left: width * 0.8,
+              top: height * 0.3,
+            }}
+            onPress={goUp}
+          >
+            <Image
+              style={{ width: width * 0.1, height: width * 0.1 }}
+              source={require('./assets/up.png')}
+            />
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={{
+              position: 'absolute',
+              left: width * 0.8,
+              top: height * 0.4,
+            }}
+            onPress={goDown}
+          >
+            <Image
+              style={{ width: width * 0.1, height: width * 0.1 }}
+              source={require('./assets/down.png')}
+            />
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={{
+              position: 'absolute',
+              left: width * 0.7,
+              top: height * 0.35,
+            }}
+            onPress={goLeft}
+          >
+            <Image
+              style={{ width: width * 0.1, height: width * 0.1 }}
+              source={require('./assets/left.png')}
+            />
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={{
+              position: 'absolute',
+              left: width * 0.9,
+              top: height * 0.35,
+            }}
+            onPress={goRight}
+          >
+            <Image
+              style={{ width: width * 0.1, height: width * 0.1 }}
+              source={require('./assets/right.png')}
+            />
+          </TouchableHighlight>
+        </View>
+      </View>
     </View>
   );
 }
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Escape the maze
 
 ## Credits
 
-Icon and splash screen use business.png from Those Icons, and donwloaded on flaticon.com
+* Icon and splash screen use business.png from Those Icons, and donwloaded on flaticon.com
+* up, down, left, right icons have been donwloaded from flaticon.com, and designed by Freepik (you can find them by searching for chevron pictures made by Freepik)
